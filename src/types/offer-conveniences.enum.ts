65c2c08590e7ce9c679cabd94f6ce02bc3bfb273
CorisMorri <nerzul33@gmail.com>
2023-05-06 22:24:14 +0300

    Приложение для терминала

---
+++ src/types/offer-conveniences.enum.ts
@@ -0,0 +1,3 @@
+export type OfferConveniences = {
+  nameConveniences: string;
+}
---
+++ src/types/offer.type.ts
@@ -0,0 +1,25 @@
+import { OfferCity } from './offer-city.enum';
+import { OfferConveniences } from './offer-conveniences.enum';
+import { OfferType } from './offer-types.enum';
+import { Photo } from './photo.type';
+import { User } from './user.type';
+
+
+export type Offer = {
+  title: string;
+  description: string;
+  postDate: Date;
+  city: OfferCity;
+  previewPath: string;
+  photosPath: Photo[];
+  flag: string;
+  rating: number;
+  type: OfferType;
+  rooms: number;
+  guests: number;
+  price: number;
+  conveniences: OfferConveniences[];
+  user: User;
+  comments: number;
+  location: string;
+}
